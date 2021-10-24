bdbb303444057146392f1eecc47b9fa948a9ce7b
ethan berk <48163588+EthanBerk@users.noreply.github.com>
2021-10-23 20:28:45 -0600

    Inital commit

---
+++ LevelManagement/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DefaultNamespace
+{
+    public class LevelManager : MonoBehaviour
+    {
+        public void GotoLevel(int scene)
+        {
+            SceneManager.LoadScene(scene);
+        }
+
+        public void NextLevel()
+        {
+            if(SceneManager.GetActiveScene().buildIndex + 1 < SceneManager.sceneCount)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void PreviousLevel()
+        {
+            if (SceneManager.GetActiveScene().buildIndex - 1 > -1)
+                SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+        }
+
+        public void ResetLevel()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        public void Exit()
+        {
+            Application.Quit();
+        }
+    }
+}
---
+++ PauseMenu/PauseMenu.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace DefaultNamespace
+{
+    public class PauseMenu : UnityEngine.MonoBehaviour
+    {
+        private AudioMixer AudioMixer;
+
+        private void Start()
+        {
+            AudioMixer.SetFloat("MasterVolume", PlayerPrefs.GetFloat("MasterVolume"));
+        }
+
+        public void SetMasterVolume(float volume)
+        {
+            AudioMixer.SetFloat("MasterVolume", volume);
+            PlayerPrefs.SetFloat("MasterVolume", volume);
+        }
+
+        public void SetFullScreen()
+        {
+            Screen.fullScreen = !Screen.fullScreen;
+        }
+    }
+}
